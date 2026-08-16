4a95d2aa3695d3445672792932b87423b117cfad
ssuzzukki <yuga.3kan@gmail.com>
2026-08-16 23:07:09 +0900

    add commands; --version, --help, build

---
+++ cmd/build.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build [file]",
+	Short: "Build Ruby application",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Build command received: %s\n", args[0])
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+
+}
